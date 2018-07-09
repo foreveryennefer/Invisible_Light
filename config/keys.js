050d76932ef49511a78c85c1e12bfa289e7e8c5e
Jason <whispersinthesoundofsilence@gmail.com>
2018-07-09 21:19:51 +0100

    Handled auth

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI:'mongodb://jason:Kendon2312@ds231941.mlab.com:31941/invisible-light',
+    googleClientID:'534623312434-9mcirv8g4v5nk5tda445h97nklbmnmh1.apps.googleusercontent.com',
+    googleClientSecret:'8dTwuiurgvl5lxTpbj2z49Nx'
+}
